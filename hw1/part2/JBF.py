b0c1d4b1308b19f9c18877a13ba6881d15c720d7
alechuang98 <alechuang98@gmail.com>
2021-03-29 23:59:10 +0800

    global output 0.4s

--- hw1/part2/JBF.py
+++ hw1/part2/JBF.py
@@ -4,9 +4,36 @@ import multiprocessing as mp
 from multiprocessing import Pool
 import ctypes
 
-share_output = None
-share_padded_img = None
-share_padded_guidance = None
+g_output = None
+g_padded_img = None
+g_padded_guidance = None
+g_output_shape = None
+g_padded_img_shape = None
+g_padded_guidance_shape = None
+g_gaussion = None
+g_exp_lookup = None
+g_pad_w = None
+
+def g_R(r, c):
+    global g_padded_guidance
+    if g_padded_guidance.ndim == 2:
+        return g_exp_lookup[abs(g_padded_guidance[r - g_pad_w : r + g_pad_w + 1, c - g_pad_w : c + g_pad_w + 1] - g_padded_guidance[r, c])]
+    else:
+        return g_exp_lookup[abs(g_padded_guidance[r - g_pad_w : r + g_pad_w + 1, c - g_pad_w : c + g_pad_w + 1, 0] - g_padded_guidance[r, c, 0])] * \
+               g_exp_lookup[abs(g_padded_guidance[r - g_pad_w : r + g_pad_w + 1, c - g_pad_w : c + g_pad_w + 1, 1] - g_padded_guidance[r, c, 1])] * \
+               g_exp_lookup[abs(g_padded_guidance[r - g_pad_w : r + g_pad_w + 1, c - g_pad_w : c + g_pad_w + 1, 2] - g_padded_guidance[r, c, 2])]
+
+def g_run(pos):
+    global g_output
+    global g_output_shape
+    output = np.frombuffer(g_output.get_obj()).reshape(g_output_shape)
+    global g_padded_img
+
+    i = pos[0] + g_pad_w
+    j = pos[1] + g_pad_w
+    gr = g_R(i, j)[:, :, np.newaxis]
+    tmp_matrix = g_gaussion * gr
+    output[i - g_pad_w][j - g_pad_w] = (tmp_matrix * g_padded_img[i - g_pad_w : i + g_pad_w + 1, j - g_pad_w : j + g_pad_w + 1]).sum(axis=tuple((0, 1))) / np.sum(tmp_matrix)
 
 class Joint_bilateral_filter(object):
     
@@ -15,19 +42,20 @@ class Joint_bilateral_filter(object):
         self.sigma_s = sigma_s
         self.wndw_size = 6*sigma_s+1
         self.pad_w = 3*sigma_s
-        self.gaussion = np.zeros((self.wndw_size, self.wndw_size))
+        global g_pad_w
+        g_pad_w = self.pad_w
+        global g_gaussion
+        g_gaussion = np.zeros((self.wndw_size, self.wndw_size))
         for i in range(self.wndw_size):
             for j in range(self.wndw_size):
-                self.gaussion[i][j] = np.exp(- ((i - self.pad_w) ** 2 + (j - self.pad_w) ** 2) / (2 * sigma_s ** 2))
-        self.gaussion = self.gaussion[:, :, np.newaxis]
+                g_gaussion[i][j] = np.exp(- ((i - self.pad_w) ** 2 + (j - self.pad_w) ** 2) / (2 * sigma_s ** 2))
+        g_gaussion = g_gaussion[:, :, np.newaxis]
         
-        self.exp_lookup = np.zeros((256))
+        global g_exp_lookup
+        g_exp_lookup = np.zeros((256))
         for i in range(256):
-            self.exp_lookup[i] = np.exp(- (i ** 2) / (2 * sigma_r ** 2 * 255 ** 2))
+            g_exp_lookup[i] = np.exp(- (i ** 2) / (2 * sigma_r ** 2 * 255 ** 2))
         print('[Build] finish!')
-        self.output_shape = None
-        self.padded_img_shape = None
-        self.padded_guidance_shape = None
 
     def get_R(self, r, c, padded_guidance):
         if padded_guidance.ndim == 2:
@@ -37,14 +65,7 @@ class Joint_bilateral_filter(object):
                    self.exp_lookup[abs(padded_guidance[r - self.pad_w : r + self.pad_w + 1, c - self.pad_w : c + self.pad_w + 1, 1] - padded_guidance[r, c, 1])] * \
                    self.exp_lookup[abs(padded_guidance[r - self.pad_w : r + self.pad_w + 1, c - self.pad_w : c + self.pad_w + 1, 2] - padded_guidance[r, c, 2])]       
 
-    def run(self, pos):
-        global share_output
-        output = np.frombuffer(share_output.get_obj()).reshape(self.output_shape)
-        global share_padded_img
-        padded_img = np.frombuffer(share_padded_img.get_obj(), dtype='int32').reshape(self.padded_img_shape)
-        global share_padded_guidance
-        padded_guidance = np.frombuffer(share_padded_guidance.get_obj(), dtype='int32').reshape(self.padded_guidance_shape)
-        
+    def run(self, pos): 
         i = pos[0] + self.pad_w
         j = pos[1] + self.pad_w
         gr = self.get_R(i, j, padded_guidance)[:, :, np.newaxis]
@@ -53,28 +74,21 @@ class Joint_bilateral_filter(object):
 
     def joint_bilateral_filter(self, img, guidance):
         BORDER_TYPE = cv2.BORDER_REFLECT
-        n = img.shape[0]
-        m = img.shape[1]
         img = img.astype('int32')
         guidance = guidance.astype('int32')
-        output = np.zeros_like(img)
-        padded_img = cv2.copyMakeBorder(img, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE)
-        padded_guidance = cv2.copyMakeBorder(guidance, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE)
+        global g_output
+        g_output = mp.Array(ctypes.c_double, img.size)
+        global g_output_shape
+        g_output_shape = img.shape
+        global g_padded_img
+        g_padded_img = cv2.copyMakeBorder(img, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE)
+        global g_padded_guidance
+        g_padded_guidance = cv2.copyMakeBorder(guidance, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE)
 
-        self.output_shape = output.shape
-        self.padded_img_shape = padded_img.shape
-        self.padded_guidance_shape = padded_guidance.shape
-
-        global share_output
-        share_output = mp.Array(ctypes.c_double, output.ravel())
-        global share_padded_img 
-        share_padded_img = mp.Array(ctypes.c_int32, padded_img.ravel())
-        global share_padded_guidance 
-        share_padded_guidance = mp.Array(ctypes.c_int32, padded_guidance.ravel())
 
         ### TODO ###
         pool = Pool()
-        pool.map(self.run, np.ndindex((img.shape[0], img.shape[1])))
+        pool.map(g_run, np.ndindex((img.shape[0], img.shape[1])))
 
         '''
         for i in range(self.pad_w, self.pad_w + n):
@@ -83,5 +97,5 @@ class Joint_bilateral_filter(object):
                 tmp_matrix = self.gaussion * gr
                 self.output[i - self.pad_w][j - self.pad_w] = (tmp_matrix * self.padded_img[i - self.pad_w : i + self.pad_w + 1, j - self.pad_w : j + self.pad_w + 1]).sum(axis=tuple((0, 1))) / np.sum(tmp_matrix)
         '''
-        output = np.frombuffer(share_output.get_obj()).reshape(self.output_shape)
+        output = np.frombuffer(g_output.get_obj()).reshape(g_output_shape)
         return np.clip(output, 0, 255).astype(np.uint8)
