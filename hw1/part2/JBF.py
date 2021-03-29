37c8f60bbb81f8219a73694373bea87bea29a62f
alechuang98 <alechuang98@gmail.com>
2021-03-29 20:12:31 +0800

    faster

--- hw1/part2/JBF.py
+++ hw1/part2/JBF.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import multiprocessing as mp
 
 
 class Joint_bilateral_filter(object):
@@ -20,13 +21,12 @@ class Joint_bilateral_filter(object):
         print('[Build] finish!')
 
     def get_R(self, r, c, padded_guidance):
-        if padded_guidance.ndim == 3:
+        if padded_guidance.ndim == 2:
+            return self.exp_lookup[abs(padded_guidance[r - self.pad_w : r + self.pad_w + 1, c - self.pad_w : c + self.pad_w + 1] - padded_guidance[r, c])]
+        else:
             return self.exp_lookup[abs(padded_guidance[r - self.pad_w : r + self.pad_w + 1, c - self.pad_w : c + self.pad_w + 1, 0] - padded_guidance[r, c, 0])] * \
                    self.exp_lookup[abs(padded_guidance[r - self.pad_w : r + self.pad_w + 1, c - self.pad_w : c + self.pad_w + 1, 1] - padded_guidance[r, c, 1])] * \
                    self.exp_lookup[abs(padded_guidance[r - self.pad_w : r + self.pad_w + 1, c - self.pad_w : c + self.pad_w + 1, 2] - padded_guidance[r, c, 2])]       
-        else:
-            return self.exp_lookup[abs(padded_guidance[r - self.pad_w : r + self.pad_w + 1, c - self.pad_w : c + self.pad_w + 1] - padded_guidance[r, c])]
-
 
     def joint_bilateral_filter(self, img, guidance):
         BORDER_TYPE = cv2.BORDER_REFLECT
@@ -41,10 +41,8 @@ class Joint_bilateral_filter(object):
         ### TODO ###
         for i in range(self.pad_w, self.pad_w + n):
             for j in range(self.pad_w, self.pad_w + m):
-                gr = np.expand_dims(self.get_R(i, j, padded_guidance), 2)
-                # gr = np.expand_dims(self.exp_lookup[abs(padded_guidance[i - self.pad_w : i + self.pad_w + 1, j - self.pad_w : j + self.pad_w + 1] - padded_guidance[i][j])], 2)
+                gr = self.get_R(i, j, padded_guidance)[:, :, np.newaxis]
                 tmp_matrix = self.gaussion * gr
-                total = np.sum(tmp_matrix)
-                output[i - self.pad_w][j - self.pad_w] = (tmp_matrix * padded_img[i - self.pad_w : i + self.pad_w + 1, j - self.pad_w : j + self.pad_w + 1]).sum(axis=tuple((0, 1))) / total
+                output[i - self.pad_w][j - self.pad_w] = (tmp_matrix * padded_img[i - self.pad_w : i + self.pad_w + 1, j - self.pad_w : j + self.pad_w + 1]).sum(axis=tuple((0, 1))) / np.sum(tmp_matrix)
 
         return np.clip(output, 0, 255).astype(np.uint8)
